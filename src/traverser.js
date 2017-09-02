7a0f36acda2f0d1dd7958ead007391b69e5b012d
Mateusz Sienkan <sin.mateusz@gmail.com>
2017-09-02 23:42:07 +0200

    Add traverser

---
+++ src/traverser.js
@@ -0,0 +1,18 @@
+const traverseArray = (array, parent, visitor) => {
+    array.forEach(child => traverseNode(child, parent, visitor))
+}
+
+const traverseNode = (node, parent, visitor) => {
+    const {type, value, body} = node
+    const {enter, exit} = visitor[type] || {}
+
+    if (enter) enter(node, parent)
+    if (body) traverseArray(body, node, visitor)
+    if (exit) exit(node, parent)
+}
+
+const traverser = (ast, visitor) => {
+    traverseNode(ast, null, visitor)
+}
+
+export default traverser
