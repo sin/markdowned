24f28a105bc2b538f509ba1d5eeee70c4c005b5d
Mateusz Sienkan <sin.mateusz@gmail.com>
2017-09-02 23:44:12 +0200

    Add transformer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { readFile } from 'fs'
 import lexer from './lexer'
 import parser from './parser'
+import transformer from './transformer'
 
 readFile('README.md', 'utf8', (err, data) => {
 
     const tokens = lexer(data)
-    const ast = parser(tokens)
+    const markdownAst = parser(tokens)
+    const htmlAst = transformer(markdownAst)
 
-    console.log(JSON.stringify(ast, null, 2))
+    console.log(JSON.stringify(htmlAst, null, 2))
 })
---
+++ src/transformer.js
@@ -0,0 +1,85 @@
+import { cloneDeep } from 'lodash'
+import traverser from './traverser'
+
+export default function transformer(ast) {
+    let clonedAst = cloneDeep(ast)
+    let newAst = {
+        type: 'HTML',
+        body: []
+    }
+
+    clonedAst._context = newAst.body
+
+    traverser(clonedAst, {
+        'TextNode': {
+            enter: function ({value}, parent) {
+                parent._context.push({
+                    type: 'TextNode',
+                    value
+                })
+            }
+        },
+        'EOL': {
+            enter: function ({value}, parent) {
+                parent._context.push({
+                    type: 'Tag',
+                    name: 'br'
+                })
+            }
+        },
+        'HeaderTag': {
+            enter: function (node, parent) {
+                node._context = []
+                parent._context.push({
+                    type: 'Tag',
+                    name: `h${Math.min(node.value.length, 6)}`,
+                    body: node._context
+                })
+            }
+        },
+        'BoldTag': {
+            enter: function (node, parent) {
+                node._context = []
+                parent._context.push({
+                    type: 'Tag',
+                    name: 'strong',
+                    body: node._context
+                })
+            }
+        },
+        'ItalicTag': {
+            enter: function (node, parent) {
+                node._context = []
+                parent._context.push({
+                    type: 'Tag',
+                    name: 'em',
+                    attrs: [
+                        {
+                            name: 'class',
+                            value: 'italic'
+                        }
+                    ],
+                    body: node._context
+                })
+            }
+        },
+        'LinkTag': {
+            enter: function (node, parent) {
+                node._context = []
+                parent._context.push({
+                    type: 'Tag',
+                    name: 'a',
+                    attrs: [
+                        {
+                            name: 'href',
+                            value: node.value
+                        }
+                    ],
+                    body: node._context
+                })
+            }
+        }
+    })
+
+    return newAst
+}
