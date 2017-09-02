5797d05acc9403201282986360050be32be7720c
Mateusz Sienkan <sin.mateusz@gmail.com>
2017-09-02 20:18:03 +0200

    Rename markdown test file to README.md

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { readFile } from 'fs'
 import lexer from './lexer'
 import parser from './parser'
 
-readFile('markdown', 'utf8', (err, data) => {
+readFile('README.md', 'utf8', (err, data) => {
 
     const tokens = lexer(data)
     const ast = parser(tokens)
