ea08d36c217f8356aa35f1ec6f6ce2d5d1d493cc
Mateusz Sienkan <sin.mateusz@gmail.com>
2017-09-03 02:41:27 +0200

    Add code generator

---
+++ src/codeGenerator.js
@@ -0,0 +1,21 @@
+import { escape } from 'lodash'
+
+const generateTextNode = (node) => escape(node.value)
+
+const generateBrNode = () => `<br />\n`
+
+const generateTagNode = ({name, attrs = [], body}) => {
+    const attrsCode = attrs.map(({name, value}) => ` ${name}='${value}'`).join('')
+    const bodyCode = generateNodes(body)
+    return `<${name}${attrsCode}>${bodyCode}</${name}>\n`
+}
+
+const generateNode = (node) => node.type === 'Tag'
+    ? (node.name === 'br' ? generateBrNode(node) : generateTagNode(node))
+    : generateTextNode(node)
+
+const generateNodes = array => array.map(node => generateNode(node)).join('')
+
+const codeGenerator = ({body}) => generateNodes(body)
+
+export default codeGenerator
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import { readFile } from 'fs'
 import lexer from './lexer'
 import parser from './parser'
 import transformer from './transformer'
+import codeGenerator from './codeGenerator'
 
 readFile('README.md', 'utf8', (err, data) => {
 
     const tokens = lexer(data)
     const markdownAst = parser(tokens)
     const htmlAst = transformer(markdownAst)
+    const html = codeGenerator(htmlAst)
 
-    console.log(JSON.stringify(htmlAst, null, 2))
+    console.log(html)
 })
